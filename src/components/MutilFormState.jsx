5066f2cab07bef6f50f0a9d12b9165403ed6f8fe
Emmanuel-222 <emmanuelogbuefi43@gmail.com>
2025-01-10 15:34:21 +0100

    This is validation of the five inputs. Coming soon...radiobutton validation 😊

--- src/components/MutilFormState.jsx
+++ src/components/MutilFormState.jsx
@@ -1,45 +1,109 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Input, RadioButton } from "../components";
 
 const MultiStateForm = () => {
-  const { register, handleSubmit, formState: {errors} } = useForm();
+  const [location, setLocation] = useState("")
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  console.log(errors)
+  console.log(errors);
   return (
     <div className="w-full p-6 text-white rounded-lg">
       <form
         onSubmit={handleSubmit((data) => {
-          console.log(data);
+          console.log({ ...data, location });
         })}
         className="space-y-2 md:space-y-6 flex justify-center items-center flex-col"
       >
         {/* Input Fields with Floating Labels */}
         <div className="flex flex-wrap md:flex-nowrap justify-center items-center w-3/4 gap-2 md:gap-4">
-          <Input type="text" input="First name" register={register} name="firstName" required/>
-          <Input type="text" name="LastName" input="Last name" register={register} required />
+          <div className="flex justify-center items-start flex-col space-y-1 w-full mb-2">
+            <Input
+              type="text"
+              input="First name"
+              register={register}
+              name="firstName"
+              required
+            />
+            <div className="h-5">
+              {errors.firstName && (
+              <span className="sm:text-[0.75rem] md:text-sm text-[#FB3748] text-[0.525rem]">
+                {errors.firstName.message}
+              </span>
+            )}
+            </div>
+            
+          </div>
+          <div className="flex justify-center items-start flex-col space-y-1 w-full mb-2">
+            <Input
+              type="text"
+              name="lastName"
+              input="Last name"
+              register={register}
+              required
+            />
+            <div className="h-5">
+              {errors.lastName && (
+                <span className="sm:text-[0.75rem] md:text-sm text-[#FB3748] text-[0.525rem]">
+                  {errors.lastName.message}
+                </span>
+              )}
+            </div>
+          </div>
         </div>
-        {/* <div className="flex flex-wrap md:flex-nowrap justify-center items-center w-3/4 gap-2 md:gap-4">
-          <Input
-            type="email"
-            name="inputValue3"
-            value={inputValue3}
-            input="Email Address"
-          />
-          <Input
-            type="text"
-            name="inputValue4"
-            value={inputValue4}
-            input="Phone Number"
-          />
+        <div className="flex flex-wrap md:flex-nowrap justify-center items-center w-3/4 gap-2 md:gap-4">
+          <div className="flex justify-center items-start flex-col space-y-1 w-full mb-2">
+            <Input
+              type="text"
+              name="emailAddress"
+              register={register}
+              input="Email Address"
+              required={true}
+              validation={{
+                pattern: {
+                  value:
+                    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                  message: "Invalid email address",
+                },
+              }}
+            />
+            <div className="h-5">
+              {errors.emailAddress && (
+                <span className="sm:text-[0.75rem] md:text-sm text-[#FB3748] text-[0.525rem]">
+                  {errors.emailAddress.message}
+                </span>
+              )}
+            </div>
+          </div>
+          <div className="flex justify-center items-start flex-col space-y-1 w-full mb-2">
+            <Input
+              type="number"
+              name="phoneNumber"
+              register={register}
+              input="Phone Number"
+              required
+            />
+            <div className="h-5">
+              {errors.phoneNumber && (
+                <span className="sm:text-[0.75rem] md:text-sm text-[#FB3748] text-[0.525rem]">
+                  {errors.phoneNumber.message}
+                </span>
+              )}
+            </div>
+          </div>
         </div>
         <div className="flex w-3/4 justify-center items-center">
           <Input
-            type="email"
-            name="inputValue5"
-            value={inputValue5}
+            type="text"
+            name="location"
             input="Location"
+            onChange={(e) => setLocation(e.target.value)}
           />
-        </div> */}
+        </div>
 
         {/* Setting Mode */}
         <div className="flex flex-col items-start w-3/4 pb-6 md:pb-12 border-b-[1px] border-[#464646] border-solid">
